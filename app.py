d788939ad32b115d0abfdb93825f23602ba40af8
Rahul Barhate <rahulbarhate97@gmail.com>
2020-01-24 19:05:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -16,10 +16,7 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.metrics import classification_report, confusion_matrix
-
-# ML Models
-
-# SVM
+import pickle
 
 #Declaring the lists for SVM 
 training_accuracy_svm = []
@@ -34,26 +31,6 @@ arrlr=[]
 resultlr = []
 
 
-# Reading the training data
-biopslides=pd.read_csv('Train.csv')
-X = biopslides.drop('y', axis=1)  
-y = biopslides['y']
-
-#Splitting the data into Train-Test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.60)
-
-#Declaring the SVM Classifier
-svclassifier = SVC(kernel='linear')  
-svclassifier.fit(X_train, y_train)
-
-
-#Declaring the Logistic Regression Classifier
-logreg = LogisticRegression(C=100).fit(X_train, y_train)
-
-
-##y_pred = svclassifier.predict(X_test)
-
-
 #Flask
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -64,12 +41,14 @@ def read_table(url):
         return [row for row in csv.DictReader(f.readlines())]
     
 APPNAME = "Identification and Quantification of Immune Stain TILs"
-STATIC_FOLDER = 'output'
-TABLE_FILE = "output/displaytabledata.csv"
+STATIC_FOLDER = 'example'
+TABLE_FILE = "example/fakecatalog.csv"
 
 
 
 table = read_table(TABLE_FILE)
+
+
 pager = Pager(len(table))
 
 
@@ -85,7 +64,7 @@ def index():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    target = os.path.join(APP_ROOT, 'processed images/')
+    target = os.path.join(APP_ROOT, 'images/')
     print(target)
     if not os.path.isdir(target):
             os.mkdir(target)
@@ -100,19 +79,24 @@ def upload():
         print ("Accept incoming file:", filename)
         print ("Save it to:", destination)
         upload.save(destination)
-        f = open("imagepath.txt", "w")
+        f = open("demofile.txt", "w")
         f.write(destination)
         f.close()
         eng.Testing(nargout=0)
         time.sleep(10)
 
+        # load the SVM Model from disk
+        svm_model, X_train, X_test, y_train, y_test = pickle.load(open('svm_model.sav', 'rb'))
+
+        # load the SVM Model from disk
+        lr_model, X_train, X_test, y_train, y_test = pickle.load(open('lr_model.sav', 'rb'))
+
         testdata = pd.read_csv('TestImage.csv')
         for index, row in testdata.iterrows():
             arrsvm.append(row)
 
-
         for i in range(len(arrsvm)):
-            resultsvm.append(svclassifier.predict([arrsvm[i]]))
+            resultsvm.append(svm_model.predict([arrsvm[i]]))
             #print(svclassifier.predict([arrsvm[i]]))
 
         for index, row in testdata.iterrows():
@@ -120,44 +104,40 @@ def upload():
 
 
         for i in range(len(arrlr)):
-            resultlr.append(logreg.predict([arrlr[i]]))
+            resultlr.append(lr_model.predict([arrlr[i]]))
             #print(logreg.predict([arrlr[i]]))
 
         ###Confusion Matrix SVM
         ##print(confusion_matrix(y_test,y_pred))  
         ##print(classification_report(y_test,y_pred))
 
-
-
-        
-
         ###Printing the accuracies for SVM
-        svmtrainacc = svclassifier.score(X_train, y_train)
-        svmtestacc = svclassifier.score(X_test, y_test)
+        svmtrainacc = svm_model.score(X_train, y_train)
+        svmtestacc = svm_model.score(X_test, y_test)
 
-        ### Print Tumor positive and negative scores using SVM
+        ### Print Tumor positive and negative scores
         positivesvm = resultsvm.count([0])
         negativesvm = resultsvm.count([1])
 
         svmresultlist = [svmtrainacc, svmtestacc, positivesvm, negativesvm]
         
-        with open ('./output/displayresultsvm.csv', 'w') as writeFileDict:
+        with open ('./example/displayresultsvm.csv', 'w') as writeFileDict:
             writer= csv.DictWriter(writeFileDict, fieldnames = ['Training Accuracy', 'Testing Accuracy', 'Positive Tumor Score', 'Negative Tumor Score'])
             writer.writeheader()
 
    
-        with open('./output/displayresultsvm.csv', 'a') as writeFile:
+        with open('./example/displayresultsvm.csv', 'a') as writeFile:
             writer = csv.writer(writeFile)
             writer.writerows([svmresultlist])
 
 
         ###Printing the accuracies for LR
-        lrtrainacc = logreg.score(X_train, y_train)
-        lrtestacc = logreg.score(X_test, y_test)
+        lrtrainacc = lr_model.score(X_train, y_train)
+        lrtestacc = lr_model.score(X_test, y_test)
         
             
 
-        ### Print Tumor positive and negative scores using LR
+        ### Print Tumor positive and negative scores
         positivelr = resultlr.count([0])
         print(positivelr)
         negativelr = resultlr.count([1])
@@ -166,12 +146,12 @@ def upload():
 
         lrresultlist = [lrtrainacc, lrtestacc, positivelr, negativelr]
         
-        with open ('./output/displayresultlr.csv', 'w') as writeFileDict:
+        with open ('./example/displayresultlr.csv', 'w') as writeFileDict:
             writer= csv.DictWriter(writeFileDict, fieldnames = ['Training Accuracy', 'Testing Accuracy', 'Positive Tumor Score', 'Negative Tumor Score'])
             writer.writeheader()
 
    
-        with open('./output/displayresultlr.csv', 'a') as writeFile:
+        with open('./example/displayresultlr.csv', 'a') as writeFile:
             writer = csv.writer(writeFile)
             writer.writerows([lrresultlist])
         
@@ -194,13 +174,13 @@ def image_view(ind=None):
 @app.route('/resultsvm<int:ind>/')
 def show_resultsvm(ind=None):
     ind=0
-    displaysvmcsv = "output/displayresultsvm.csv"
+    displaysvmcsv = "example/displayresultsvm.csv"
     svmtable = read_table(displaysvmcsv)
     return render_template('displaysvmresults.html', index = ind, pager=pager, data = svmtable[ind])
 
 @app.route('/resultlr<int:ind>/')
 def show_resultlr(ind=None):
-    displaylrcsv = "output/displayresultlr.csv"
+    displaylrcsv = "example/displayresultlr.csv"
     lrtable = read_table(displaylrcsv)
     return render_template('displaylrresults.html', index = ind, pager=pager, data = lrtable[ind])
 
@@ -210,7 +190,7 @@ def goto():
 
 @app.route('/gallery')
 def get_gallery():
-    image_names = os.listdir('./output/images')
+    image_names = os.listdir('./example/images')
     print(image_names)
     return render_template('gallery.html', image_names=image_names)
 
